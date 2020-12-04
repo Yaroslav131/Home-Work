65bbbb615013776ab6086ce3a82ef29a128c0f29
Yaroslav131 <72652633+Yaroslav131@users.noreply.github.com>
2020-12-04 22:14:53 +0300

    Add addition in tutorial.

--- Prinsess/Prinsess/Output.cs
+++ Prinsess/Prinsess/Output.cs
@@ -11,7 +11,9 @@ namespace LineOutput
                 "Rules of game:\n" +
                 "1)Hero have 10 HP.\n" +
                 "2)Field have 10 mins on itself, with random damage from 1 to 10.\n" +
-                "3)Mins explode ones time.");
+                "3)Mins explode ones time.\n" +
+                "Control:\n" +
+                "You can move Hero with AWSD or arrows or NumPad.");
             WriteLine("------------------------------");
         }
     }
