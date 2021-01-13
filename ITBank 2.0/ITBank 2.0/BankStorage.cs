324d6efef8b7939e3a171929004b903958c51cd7
Yaroslav131 <72652633+Yaroslav131@users.noreply.github.com>
2021-01-13 08:24:38 +0300

    Fix space.

--- ITBank 2.0/ITBank 2.0/BankStorage.cs
+++ ITBank 2.0/ITBank 2.0/BankStorage.cs
@@ -6,7 +6,6 @@ namespace ITBank_2._0
 {
     class BankStorage
     {
-
         public static int[] DebitCards { get; set; }
         public static int[] CreditCards { get; set; }
         public static double[] AccountsScore { get; set; }
